f24dcc170426209f5e6f162c93f1edc8b3a1eb73
7uppp <markwang9315@gmail.com>
2023-09-07 19:43:12 +1000

    add size

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -8,21 +8,33 @@ const NavBar = () => {
     <div className={'navbar_container w-full mt-[1.375rem]'}>
       <div className={'navbar_wrapper flex justify-between items-center'}>
         <div>
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" className="w-[13.5625rem]" />
         </div>
         <div
           className={
             'social_platform_container flex justify-between items-center gap-[1.875rem]'
           }>
           <a href="">
-            <img src={discord} alt="discord" className={'cursor-pointer'} />
+            <img
+              src={discord}
+              alt="discord"
+              className={'cursor-pointer w-[2.375rem]'}
+            />
           </a>
           <a href="">
-            <img src={twitter} alt="twitter" className={'cursor-pointer'} />
+            <img
+              src={twitter}
+              alt="twitter"
+              className={'cursor-pointer w-[2.375rem'}
+            />
           </a>
 
           <a href="">
-            <img src={telegram} alt="telegram" className={'cursor-pointer'} />
+            <img
+              src={telegram}
+              alt="telegram"
+              className={'cursor-pointer w-[2.375rem'}
+            />
           </a>
         </div>
       </div>
