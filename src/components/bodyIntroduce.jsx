a2a9b424adfed92214341b0b80e9d8581042433b
7uppp <markwang9315@gmail.com>
2023-09-07 13:19:51 +1000

    fix css

--- src/components/bodyIntroduce.jsx
+++ src/components/bodyIntroduce.jsx
@@ -53,97 +53,131 @@ const BodyIntroduce = () => {
             <motion.div
               whileHover={{ scale: 1.2 }}
               className={
-                'list_nft-into-boxes flex flex-col justify-center items-center gap-[60px]'
+                'list_nft-into-boxes flex justify-center items-center'
               }>
-              <img src={listnft} alt="listnft" />
-              <span className={'text-[24px] font-semibold text-center'}>
-                List NFTs into boxes
-              </span>
-              <p
-                className={'w-[353px] whitespace-wrap text-[18px] text-center'}>
-                List NFTs like what you do on other NFT marketplace, Rollinghub
-                will issue tickets for you.
-              </p>
+              <div className="flex flex-col gap-[20px] ">
+                <img src={listnft} alt="listnft" className="w-[96px] mx-auto" />
+                <span
+                  className={'text-[24px] font-semibold text-center h-[64px]'}>
+                  List NFTs into boxes
+                </span>
+                <p
+                  className={
+                    'whitespace-wrap text-[18px] text-center h-[60px]'
+                  }>
+                  List NFTs like what you do on other NFT marketplace,
+                  Rollinghub will issue tickets for you.
+                </p>
+              </div>
             </motion.div>
 
             <motion.div
               whileHover={{ scale: 1.2 }}
-              className={
-                'share_box  flex flex-col justify-center items-center gap-[43px]'
-              }>
-              <img src={share} alt="shareIcon" />
-              <span className={'text-[24px] font-semibold text-center'}>
-                Share box link to friends or communities
-              </span>
-              <p
-                className={'w-[353px] whitespace-wrap text-[18px] text-center'}>
-                It's a good idea to call friends and communities to take tickets
-                from the box you created.
-              </p>
+              className={'share_box  flex  justify-center items-center '}>
+              <div className="flex flex-col gap-[20px] ">
+                <img src={share} alt="shareIcon" className="w-[96px] mx-auto" />
+                <span
+                  className={'text-[24px] font-semibold text-center h-[64px]'}>
+                  Share box link to friends or communities
+                </span>
+                <p
+                  className={
+                    'w-[353px] whitespace-wrap text-[18px] text-center h-[60px]'
+                  }>
+                  It's a good idea to call friends and communities to take
+                  tickets from the box you created.
+                </p>
+              </div>
             </motion.div>
 
             <motion.div
               whileHover={{ scale: 1.2 }}
-              className={
-                'withdraw_eth  flex flex-col justify-center items-center gap-[75px]'
-              }>
-              <img src={dollar} alt="dollarIcon" />
-              <span className={'text-[24px] font-semibold text-center '}>
-                Withdraw ETH
-              </span>
-              <p
-                className={'w-[353px] whitespace-wrap text-[18px] text-center'}>
-                You can withdraw ETH from boxes sold out.
-              </p>
+              className={'withdraw_eth flex justify-center items-center'}>
+              <div className="flex flex-col gap-[20px]">
+                <img
+                  src={dollar}
+                  alt="dollarIcon"
+                  className="w-[96px] mx-auto"
+                />
+                <span
+                  className={'text-[24px] font-semibold text-center h-[64px] '}>
+                  Withdraw ETH
+                </span>
+                <p
+                  className={
+                    'w-[353px] whitespace-wrap text-[18px] text-center h-[60px] '
+                  }>
+                  You can withdraw ETH from boxes sold out.
+                </p>
+              </div>
             </motion.div>
           </div>
         ) : (
           <div className={'buy_tickets_contents flex gap-[165px] mt-[70px]'}>
             <motion.div
               whileHover={{ scale: 1.2 }}
-              className={
-                'find_nfts flex flex-col justify-center items-center gap-[80px]'
-              }>
-              <img src={find} alt="findnft" />
-              <span className={'text-[24px] font-semibold text-center'}>
-                Find NFTs & Buy tickets
-              </span>
-              <p
-                className={'w-[353px] whitespace-wrap text-[18px] text-center'}>
-                You can find NFTs, and buy tickets of the corresponding box to
-                win them.
-              </p>
+              className={'find_nfts flex justify-center items-center ]'}>
+              <div className="flex flex-col gap-[20px] ">
+                <img src={find} alt="findnft" className="w-[96px] mx-auto" />
+                <span
+                  className={'text-[24px] font-semibold text-center h-[64px]'}>
+                  Find NFTs & Buy tickets
+                </span>
+                <p
+                  className={
+                    'w-[353px] whitespace-wrap text-[18px] text-center h-[60px]'
+                  }>
+                  You can find NFTs, and buy tickets of the corresponding box to
+                  win them.
+                </p>
+              </div>
             </motion.div>
 
             <motion.div
               whileHover={{ scale: 1.2 }}
-              className={
-                'share_box  flex flex-col justify-center items-center gap-[75px]'
-              }>
-              <img src={share2} alt="shareIcon" />
-              <span className={'text-[24px] font-semibold text-center'}>
-                Share box link to friends or communities
-              </span>
-              <p
-                className={'w-[353px] whitespace-wrap text-[18px] text-center'}>
-                Help the box to draw.
-              </p>
+              className={'share_box flex justify-center items-center'}>
+              <div className="flex flex-col gap-[20px]">
+                <img
+                  src={share2}
+                  alt="shareIcon"
+                  className="w-[96px] mx-auto"
+                />
+                <span
+                  className={'text-[24px] font-semibold text-center h-[64px]'}>
+                  Share box link to friends or communities
+                </span>
+                <p
+                  className={
+                    'w-[353px] whitespace-wrap text-[18px] text-center h-[60px]'
+                  }>
+                  Help the box to draw.
+                </p>
+              </div>
             </motion.div>
 
             <motion.div
               whileHover={{ scale: 1.2 }}
-              className={
-                'withdraw_prize  flex flex-col justify-center items-center gap-[80px]'
-              }>
-              <img src={withdraw} alt="withdraw" />
-              <span className={'text-[24px] font-semibold text-center '}>
-                Withdraw prizes
-              </span>
-              <p
-                className={'w-[353px] whitespace-wrap text-[18px] text-center'}>
-                You can withdraw NFTs you win to your wallets, or list them in a
-                box directly.
-              </p>
+              className={'withdraw_prize flex justify-center items-center'}>
+              <div className="flex flex-col  gap-[20px]">
+                <img
+                  src={withdraw}
+                  alt="withdraw"
+                  className="w-[96px] mx-auto"
+                />
+                <span
+                  className={
+                    'text-[24px] font-semibold text-center  h-[64px] '
+                  }>
+                  Withdraw prizes
+                </span>
+                <p
+                  className={
+                    'w-[353px] whitespace-wrap text-[18px] text-center  h-[60px]'
+                  }>
+                  You can withdraw NFTs you win to your wallets, or list them in
+                  a box directly.
+                </p>
+              </div>
             </motion.div>
           </div>
         )}
