8b09cc22716595a776854a17a9f41ecd68319f19
7uppp <markwang9315@gmail.com>
2023-09-14 20:31:54 +1000

    add loading page and effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
+import { useEffect, useState } from 'react'
 import Navbar from './components/navBar.jsx'
 import Introduce from './components/introduce.jsx'
 import BodyIntroduce from './components/bodyIntroduce.jsx'
 import NewsLetter from './components/newsLetter.jsx'
+import LoadingPage from './components/loadingPage.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 4000)
+  }, [])
+
   return (
-    <ChakraProvider>
+    <>
       <div
-        className={
-          'w-[118.75rem] flex flex-col mx-auto justify-center items-center overflow-hidden'
-        }>
-        <div
-          className={
-            ' bg-hero-pattern bg-no-repeat bg-cover h-[68.3125rem] px-[14.875rem]'
-          }>
-          <div className={'flex flex-col'}>
-            <Navbar />
-            <Introduce />
-          </div>
-        </div>
-        <BodyIntroduce />
-        <NewsLetter />
-        <div
-          className={
-            ' w-full h-[3.125rem] border border-1 bg-purple-950 mt-[9.125rem] '
-          }></div>
+        className={`fixed top-0 left-0 w-full h-full z-10 ${
+          isLoading ? 'fade-in' : 'fade-out'
+        }`}>
+        <LoadingPage />
       </div>
-    </ChakraProvider>
+      <ChakraProvider>
+        <div
+          className={`w-[118.75rem] flex flex-col mx-auto justify-center items-center overflow-hidden ${
+            isLoading ? 'fade-out' : 'fade-in'
+          }`}>
+          <div className="bg-hero-pattern bg-no-repeat bg-cover h-[68.3125rem] px-[14.875rem]">
+            <div className="flex flex-col">
+              <Navbar />
+              <Introduce />
+            </div>
+          </div>
+          <BodyIntroduce />
+          <NewsLetter />
+          <div className="w-full h-[3.125rem] border border-1 bg-purple-950 mt-[9.125rem]"></div>
+        </div>
+      </ChakraProvider>
+    </>
   )
 }
 
---
+++ src/components/loadingPage.jsx
@@ -0,0 +1,15 @@
+import './loadingPage.css'
+
+const loadingPage = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <h1
+        data-spotlight="Unlock the Future of NFT Liquidity with RollingHub"
+        className="text-[5rem]">
+        Unlock the Future of NFT Liquidity with RollingHub
+      </h1>
+    </div>
+  )
+}
+
+export default loadingPage
