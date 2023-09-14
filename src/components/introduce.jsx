83dde34a40b0cffb1397f12ee9bb7625db1f8ef0
7uppp <markwang9315@gmail.com>
2023-09-14 22:03:49 +1000

    update css

--- src/components/introduce.jsx
+++ src/components/introduce.jsx
@@ -21,25 +21,25 @@ const Introduce = () => {
 
   return (
     <div className={'introduce_container relative mt-[11.5625rem] '}>
-      <div className={' introduce_wrapper grid grid-cols-2 gap-x-20 '}>
-        <div className={'content  flex flex-col justify-center'}>
-          <div
+      <div className={' introduce_wrapper grid grid-cols-2 gap-x-30 '}>
+        <div className={'content flex flex-col justify-center'}>
+          {/* <div
             className={
               'introduce_content_top max-w-[35.625rem] border-2 rounded-full  '
             }>
             <span
               className={
-                'text-3xl text-center text-white tracking-wider pl-[1.875rem] pr-[1rem] pt-[1.125rem] pb-[1rem] box-border '
+                'text-3xl text-center text-white tracking-wider pl-[1.875rem] pr-[1rem] pt-[1.125rem] pb-[1rem] box-border ] '
               }>
               INTRODUCING ROLLINGHUB
             </span>
-          </div>
+          </div> */}
           <div className={'the_first_raffle_marketplace w-[43.75rem]'}>
             <span className={'text-white text-[4.5rem]'}>
               <TypeWriter />
             </span>
           </div>
-          <div className={'introducing_sentence w-[41.4375rem] mt-[1.25rem] '}>
+          <div className={'introducing_sentence w-[41.4375rem] mt-[3rem] '}>
             <p className={'text-white whitespace-wrap tracking-wider'}>
               Rollinghub is an NFT marketplace designed for collectors to unlock
               rare digital assets by raffle, and meanwhile create their own
