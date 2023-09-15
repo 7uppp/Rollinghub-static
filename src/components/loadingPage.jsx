e35ef09ae3a85bd70dea0802ed555350534f3411
7uppp <markwang9315@gmail.com>
2023-09-15 20:01:22 +1000

    change loading page css

--- src/components/loadingPage.jsx
+++ src/components/loadingPage.jsx
@@ -5,7 +5,7 @@ const loadingPage = () => {
     <div className="flex justify-center items-center w-full h-screen">
       <h1
         data-spotlight="Unlock the Future of NFT Liquidity with RollingHub"
-        className="text-[5rem]">
+        className="text-[5rem] pr-[0.1875rem]">
         Unlock the Future of NFT Liquidity with RollingHub
       </h1>
     </div>
